8be69efb544845a83174ce7985fb57f0946d3ba3
Ygor Garofalo <igorgarofalo@gmail.com>
2023-11-27 14:50:20 +0100

    ex1 ok

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,35 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+
 public class Main {
+
+    static String[] stringArray = {"This", "is", "my", "first", "javaCode"};
+    static String word1 = "project";
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int multipliedResult = mult(4, 8);
+        System.out.println("the multiplied result is " + multipliedResult);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        concatTypes("Età: ", 28);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        String[] newArray = insertWordinArray(stringArray, word1);
+        System.out.println(Arrays.toString(newArray));
+    }
+
+    public static int mult(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static void concatTypes(String word, int num) {
+        System.out.println((word + num));
+    }
+
+    public static String[] insertWordinArray(String[] stringArray, String word1) {
+        String[] newArray = new String[6];
+        System.arraycopy(stringArray, 0, newArray, 0, 2);
+        newArray[2] = word1;
+        newArray[3] = stringArray[3];
+        newArray[4] = stringArray[4];
+        newArray[5] = "";
+        return newArray;
     }
 }
